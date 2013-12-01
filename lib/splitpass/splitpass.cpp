52772021ba34b5ee6fe6dcbb9aeed5c185ca9f0c
ruoran <ruoran@eecs583a.eecs.umich.edu>
2013-12-01 00:26:56 -0500

    tring to add a copy of the loop

--- lib/splitpass/splitpass.cpp
+++ lib/splitpass/splitpass.cpp
@@ -4,9 +4,16 @@
 #include "llvm/Pass.h"
 #include "llvm/IR/Function.h"
 #include "llvm/Support/raw_ostream.h"
+#include "llvm/Transforms/Scalar.h"
+#include "llvm/Transforms/Utils/Local.h"
+#include "llvm/Transforms/Utils/SSAUpdater.h"
 #include <ios>  
 #include <fstream>
 #include <iomanip>
+
+#include "llvm/Transforms/Utils/Cloning.h" // inorder to include "ValueToValueMapTy"
+#include "llvm/Transforms/Utils/BasicBlockUtils.h" // splitEdge, etc
+
 
 /* Loop is a child class of LoopBase
  * There are many useful methods defined in LoopBase
@@ -15,6 +22,54 @@
 using namespace llvm;
 
 namespace {
+
+    /* Note about LLPassManager::insertLoop(Loop *L, Loop *ParentLoop)
+     *
+     * If ParentLoop is NULL, it will be inserted as a TopLevelLoop
+     *
+     */
+     
+    
+    // CreateOneLoop should take the current Loop and a SSC List, and will create loop based on that
+    static Loop *CreateOneLoop(Loop *L, LPPassManager *LPM) {
+        ValueToValueMapTy VMap;  // store old bb -> new bb  TODO maybe init out of this func
+        
+        Loop *new_loop = new Loop();
+    
+        
+        BasicBlock *entrybb = L->getLoopPreheader();  // entry = preheader
+        BasicBlock *exitbb = L->getExitBlock(); 
+        std::vector< BasicBlock * > body = L->getBlocks();
+    
+        //entrybb->getBasicBlockList()->dump();//.push_back(body[1]);
+
+        //BasicBlock *testBB = SplitEdge(entrybb, entrybb->getSinglePredecessor, SplitPass);
+    
+        BasicBlock *new_entrybb = CloneBasicBlock(entrybb, VMap);
+        BasicBlock *new_exitbb = CloneBasicBlock(exitbb, VMap);
+    
+        // addBlockEntry means add a basic block
+        new_loop->addBlockEntry(new_entrybb);
+        new_loop->addBlockEntry(new_exitbb);
+    
+        for(std::vector<BasicBlock * >::iterator it = body.begin(); it != body.end(); ++it) {
+            /* std::cout << *it; ... */
+            BasicBlock *new_bb = CloneBasicBlock(dyn_cast<BasicBlock>(*it), VMap);
+            new_loop->addBlockEntry(new_bb);
+        }
+    
+         // Add all of the blocks in L to the new loop.
+        //for (Loop::block_iterator I = L->block_begin(), E = L->block_end(); I != E; ++I) {
+        //    if (LI->getLoopFor(*I) == L) {
+        //        new_loop->addBasicBlockToLoop(new_bb, LI->getBase());
+        //    }
+        //}
+        
+       
+        LPM->insertLoop(new_loop, NULL); // second parameter means insert as TopLevelLoop
+        return new_loop;
+    }
+
     struct SplitPass: public LoopPass {
         // init
         static char ID;
@@ -70,7 +125,19 @@ namespace {
             BasicBlock *exitbb = L->getExitBlock();
             exitbb->dump();
 
+            //BasicBlock *testBB = llvm::SplitEdge(preheader, latch, this);
+
+
+            //Loop* lp = CreateOneLoop(L, &LPM);
             return false; // TODO if code is chaged, should return true
+        }
+
+        bool doFinalization() {
+            //BasicBlock *preheader = L->getLoopPreheader();
+            //BasicBlock *latch = L->getLoopLatch();
+
+            //BasicBlock *testBB = SplitEdge(preheader, latch, this);
+            return true;
         }
 
         // Load other analysis
@@ -79,28 +146,6 @@ namespace {
             AU.addRequired<ProfileInfo>();
         } 
     };
-}
-
-/* Note about LLPassManager::insertLoop(Loop *L, Loop *ParentLoop)
- *
- * If ParentLoop is NULL, it will be inserted as a TopLevelLoop
- *
- */
- 
-
-// CreateOneLoop should take the current Loop and a SSC List, and will create loop based on that
-static Loop *CreateOneLoop(Loop *L, LPPassManager *LPM) {
-    Loop *new_loop = new Loop();
-    LPM->insertLoop(new_loop, NULL); // second parameter means insert as TopLevelLoop
-
-    //L->get
-
-     // Add all of the blocks in L to the new loop.
-    //for (Loop::block_iterator I = L->block_begin(), E = L->block_end(); I != E; ++I) {
-    //    if (LI->getLoopFor(*I) == L) {
-    //        new_loop->addBasicBlockToLoop(new_bb, LI->getBase());
-    //    }
-    //}
 }
 
 char SplitPass::ID = 0;
