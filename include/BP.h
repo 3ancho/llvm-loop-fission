03f620118c971d49345891cb007f9e99ceff51a4
caogao <caogao@umich.edu>
2013-12-16 00:36:08 -0500

    check_partition_0

--- include/BP.h
+++ include/BP.h
@@ -40,7 +40,9 @@ typedef std::map<Loop*, std::vector<std::vector<Instruction*> > > loop_sccs;
 //////functions/////
     inst_vec dfs(Instruction *start_inst, inst_map_set dg_of_loop, inst_set all_insts, inst_visit *visited);
     inst_vec_vec build_partition(Loop *CurL, inst_map_set CurInstMapSet);
+    inst_vec_vec check_partition(inst_vec_vec old_scc);
     inst_map_set dual_dg_map(inst_map_set dg_inst_map);
+    int NumHeaderInst(Loop *L);
 
     std::map<Loop*, int> NumOfPartitions;
     loop_sccs	Partitions; 
