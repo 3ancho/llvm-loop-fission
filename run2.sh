fe9145ce9841752cb5ef705f3d826e274868f359
Ruoran Wang <ruoranwang@gmail.com>
2013-12-17 12:45:42 -0500

    updated loop4 in master

--- run2.sh
+++ run2.sh
@@ -10,8 +10,8 @@ clang -emit-llvm -o $fname.bc -c $fpath
 echo "opt -indvars -instcombine -loop-simplify -simplifycfg -mem2reg $fname.bc > $fname.simple.bc"
 opt -indvars -instcombine -loop-simplify -simplifycfg -mem2reg $fname.bc > $fname.simple.bc
 
-echo "opt -load Release+Asserts/lib/splitpass.so -basicaa -da -dg -splitpass_scc < $fname.simple.bc > $fname.splited.bc "
-opt -debug -load Release+Asserts/lib/splitpass.so -basicaa -da -dg -bp -splitpass2 < $fname.simple.bc > $fname.splited.bc 
+echo "opt -load Release+Asserts/lib/splitpass.so -basicaa -da -dg -splitpass2 < $fname.simple.bc > $fname.splited.bc "
+opt -debug -load Release+Asserts/lib/splitpass.so -basicaa -da -dg -bp -splitpass2< $fname.simple.bc > $fname.splited.bc 
 
 test -e $fname.bc && rm $fname.bc
 llvm-dis $fname.simple.bc
